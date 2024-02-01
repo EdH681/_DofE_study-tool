f85121a28f30e55461366db5f92803e139c2fa3d
Edward <edwardhornby07@gmail.com>
2024-02-01 19:16:19 +0000

    added the functions to add items and check results

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import tkinter
+import random
 
 
 def load(subject, topic):
@@ -12,6 +13,32 @@ def load(subject, topic):
     return questions
 
 
-questions = load("computing", "databases")
+def check(attempt, answer, threshold=2):
+    correct = 0
+    for i in attempt:
+        if i in answer:
+            print(i)
+            correct += 1
+            answer = answer.replace(i, "")
+            print(answer)
+    threshold = len(answer) - threshold
 
+    return correct > threshold
+
+
+def add(subject, title, filename):
+    with open(f"{subject}.json", "r") as retrieve:
+        temp = json.load(retrieve)
+
+    new = {}
+    with open(f"{filename}", "r") as take:
+        for line in take:
+            line = line.strip()
+            line = line.split(",")
+            new[line[0]] = line[1]
+
+    temp[title] = new
+
+    with open(f"{subject}.json", "w") as update:
+        json.dump(temp, update)
 
