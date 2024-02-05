a5054ad99bdb2b085710258040e4d8821e224a87
Edward <edwardhornby07@gmail.com>
2024-02-05 21:56:27 +0000

    began work on the ui

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 import tkinter
+import tkinter.ttk as ttk
+import os
 import random
+from math import *
 
 
 def load(subject, topic):
@@ -14,14 +17,17 @@ def load(subject, topic):
 
 
 def check(attempt, answer, threshold=2):
+    answer = answer.replace(" ", "")
+    attempt = attempt.replace(" ", "")
+    threshold = len(answer) - threshold
+    answer = [*answer]
     correct = 0
     for i in attempt:
         if i in answer:
-            print(i)
             correct += 1
-            answer = answer.replace(i, "")
-            print(answer)
-    threshold = len(answer) - threshold
+            answer.remove(i)
+        else:
+            print(i)
 
     return correct > threshold
 
@@ -42,3 +48,133 @@ def add(subject, title, filename):
     with open(f"{subject}.json", "w") as update:
         json.dump(temp, update)
 
+
+def clear(container):
+    for item in container.winfo_children():
+        item.destroy()
+
+
+def menu(frame):
+    clear(frame)
+    baseY = 200
+    titleText = "Select an Option"
+    title = tkinter.Label(frame, text=titleText, font=("Helvetica", 20, "bold"))
+    title.place(relx=0, y=20, width=500, height=50)
+
+    # Test Button
+    startText = "Test Yourself"
+    testYourself = tkinter.Button(frame, text=startText, font=("Helvetica", 20), command=lambda: selection(root))
+    testYourself.place(x=50, y=baseY, width=400, height=50)
+
+    # Add Button
+    addText = "Add More Question Sets"
+    addButton = tkinter.Button(frame, text=addText, font=("Helvetica", 20))
+    addButton.place(x=50, y=baseY + 55, width=400, height=50)
+
+    # View Button
+    viewText = "View All Question Sets"
+    viewButton = tkinter.Button(frame, text=viewText, font=("Helvetica", 20))
+    viewButton.place(x=50, y=baseY + 110, width=400, height=50)
+
+    # Remove Button
+    removeText = "Remove a Question Set"
+    removeButton = tkinter.Button(frame, text=removeText, font=("Helvetica", 20))
+    removeButton.place(x=50, y=baseY + 165, width=400, height=50)
+
+
+def selection(frame):
+    clear(frame)
+
+    # Getting Subjects
+    options = ["Select an option"]
+    for subject in os.listdir():
+        if subject.split(".")[1] == "json":
+            options.append(subject.split(".")[0].title())
+
+    # Title
+    title = tkinter.Label(frame, text="Select a Subject and a Topic", font=("Helvetica", 20, "bold"))
+    title.pack(pady=10)
+
+    # Topic Box
+    tkinter.Label(frame, text="Select a Topic", font=("Helvetica", 12, "bold")).place(x=105, y=215)
+    topicSelect = ttk.Combobox(frame, values=["Select an Option"], state="disabled", width=42)
+    topicSelect.current(0)
+    topicSelect.place(x=105, y=250)
+
+    # Subject Box
+    tkinter.Label(frame, text="Select a Subject", font=("Helvetica", 12, "bold")).place(x=105, y=115)
+    subjectSelect = ttk.Combobox(frame, values=options, width=20, font=("Helvetica", 10), state="readonly")
+    subjectSelect.current(0)
+    subjectSelect.place(x=105, y=150)
+
+    # Subject Button
+    subjectButton = tkinter.Button(frame, text="Select",
+                                   command=lambda: subject_check(subjectSelect.get(), topicSelect))
+    subjectButton.place(x=275, y=150, width=100, height=22)
+
+    # Start Button
+    startButton = tkinter.Button(frame, text="Start", font=("Helvetica", 20), command=lambda: \
+        questionGrab(frame, subjectSelect.get(), topicSelect.get()))
+    startButton.place(x=105, y=350, width=280, height=50)
+
+    # Return to Menu
+    returnButton = tkinter.Button(frame, text="menu", command=lambda: menu(frame))
+    returnButton.place(x=10, y=470, width=50, height=25)
+
+
+def subject_check(value, box):
+    if value != "Select an option":
+        box["state"] = "readonly"
+        topics = ["Select an option"]
+        with open(f"{value}.json", "r") as topicList:
+            data = json.load(topicList)
+            for topic in data:
+                topics.append(topic)
+        box["values"] = topics
+    else:
+        box['state'] = "disabled"
+        box["values"] = ["Select an Option"]
+        box.current(0)
+
+
+def questionGrab(frame, subject, topic):
+    try:
+        questions = load(subject.lower(), topic.lower())
+    except KeyError:
+        tkinter.Label(frame, text="Select a valid topic", fg="red").place(x=105, y=400, width=280)
+    except FileNotFoundError:
+        tkinter.Label(frame, text="Select a valid subject", fg="red").place(x=105, y=400, width=280)
+    else:
+        quiz(frame, questions)
+
+
+def quiz(frame, questions):
+    clear(frame)
+    questionNumber = 1
+    random.shuffle(questions)
+    title = tkinter.Label(frame, text=f"Question {questionNumber}", font=("Helvetica", 20, "bold"))
+    title.place(x=0, y=10, width=500, height=50)
+
+    questionAnswer = questions.pop(0)
+    question = tkinter.Label(frame, text=questionAnswer[0], font=("Helvetica", 18), wraplength=455)
+    question.place(x=0, y=100, width=500, height=150)
+
+    answerBox = tkinter.Entry(frame, font=("Helvetica", 18), justify="center")
+    answerBox.place(x=50, y=300, height=50, width=400)
+
+    submitButton = tkinter.Button(frame, text="Submit", font=("Helvetica", 15), command=lambda: quiz_check(answerBox.get(), questionAnswer[1]))
+    submitButton.place(x=150, y=360, height=40, width=200)
+
+
+def quiz_check(entered, correct):
+    right = check(entered.lower(), correct)
+    if right:
+        print("Correct")
+
+# setting up the tkinter window
+root = tkinter.Tk()
+root.geometry("500x500")
+root.resizable(False, False)
+menu(root)
+
+root.mainloop()
